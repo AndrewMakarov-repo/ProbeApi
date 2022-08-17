4cc799db69c5f98c467a15cb3983f7cf7067d548
Andrey Makarov <andremkr@tut.by>
2022-08-17 17:12:49 +0300

    Add project files.

---
+++ ProbeApi/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace ProbeApi
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [TestCase("nl", "1020", HttpStatusCode.OK)]
+        [TestCase("li", "0005", HttpStatusCode.NotFound)]
+        [TestCase("lt", "00001", HttpStatusCode.OK)]
+        [TestCase("ru", "1000", HttpStatusCode.NotFound)]
+        [TestCase("ru", "5", HttpStatusCode.NotFound)]
+        public void StatusCodeTest(string countryCode, string zipCode, HttpStatusCode expectedHttpStatusCode)
+        {
+            // arrange
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest($"{countryCode}/{zipCode}", Method.Get);
+            
+            // act
+            var response = client.Execute(request);
+
+            // assert
+            Assert.That(response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+        }
+    }
+}
